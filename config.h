a4decd1ce70ea95c0d77cc18d4d5b5a05933657b
Robert <robertkeen9@gmail.com>
2019-11-19 19:38:45 -0800

    added keyboard shortcuts for volume adjustment

--- config.h
+++ config.h
@@ -59,12 +59,16 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *browsercmd[] = { "google-chrome", NULL};
+static const char *volumeup[]  = { "amixer", "-D", "default", "sset", "Master", "1%+", "unmute", NULL };
+static const char *volumedown[]  = { "amixer", "-D", "default", "sset", "Master", "1%-", "unmute", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY|ShiftMask,		XK_b,      spawn,          {.v = browsercmd } },
+	{ MODKEY,                       0x5d,      spawn,          {.v = volumeup } },
+	{ MODKEY,                       0x5b,      spawn,          {.v = volumedown } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
