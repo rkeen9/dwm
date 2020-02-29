5828a917168526058545eca08168ceb1521768bd
Robert Keen <robertkeen9@gmail.com>
2020-02-28 20:43:22 -0800

    gaps can now be disabled with alt+g

--- config.h
+++ config.h
@@ -59,13 +59,13 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
+	{ "[]=",      gaps },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
     { "|M|",      centeredmaster },
     { ">M>",      centeredfloatingmaster }, 
 	{ "|||",      col },
-    { "[F]=",     fakefull },
+    { "[G]=",     tile },
 };
 
 /* key definitions */
@@ -138,6 +138,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_t,                   setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,                   setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,                   setlayout,      {.v = &layouts[2]} },
+    { MODKEY,                       XK_g,                   setlayout,      {.v = &layouts[6]} },
 	{ MODKEY,                       XK_space,               setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,               togglefloating, {0} },
 	{ MODKEY,                       XK_0,                   view,           {.ui = ~0 } },
--- dwm.c
+++ dwm.c
@@ -221,7 +221,7 @@ static void spawn(const Arg *arg);
 static void tag(const Arg *arg);
 static void tagmon(const Arg *arg);
 static void tile(Monitor *);
-static void fakefull(Monitor *);
+static void gaps(Monitor *);
 static void togglebar(const Arg *arg);
 static void togglefloating(const Arg *arg);
 static void toggletag(const Arg *arg);
@@ -1762,6 +1762,41 @@ tagmon(const Arg *arg)
 void
 tile(Monitor *m)
 {
+	unsigned int i, n, h, mw, my, ty;
+    float mfacts = 0, sfacts = 0; 
+	Client *c;
+
+    for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++) {
+        if (n < m->nmaster)
+            mfacts += c->cfact;
+        else
+            sfacts += c->cfact;
+    }
+
+	if (n == 0)
+		return;
+
+	if (n > m->nmaster)
+		mw = m->nmaster ? m->ww * m->mfact : 0;
+	else
+		mw = m->ww;
+	for (i = my = ty = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++)
+		if (i < m->nmaster) {
+            h = (m->wh - my) * (c->cfact / mfacts); 
+			resize(c, m->wx, m->wy + my, mw - (2*c->bw), h - (2*c->bw), 0);
+			my += HEIGHT(c);
+            mfacts -= c->cfact; 
+		} else {
+            h = (m->wh - ty) * (c->cfact / sfacts);
+			resize(c, m->wx + mw, m->wy + ty, m->ww - mw - (2*c->bw), h - (2*c->bw), 0);
+			ty += HEIGHT(c);
+            sfacts -= c->cfact; 
+		}
+}
+
+void
+gaps(Monitor *m)
+{
     unsigned int i, n, h, mw, my, ty, ns;
     float mfacts = 0, sfacts = 0; 
 	Client *c;
@@ -1797,40 +1832,6 @@ tile(Monitor *m)
 		}
 }
 
-void
-fakefull(Monitor *m)
-{
-	unsigned int i, n, h, mw, my, ty;
-    float mfacts = 0, sfacts = 0; 
-	Client *c;
-
-    for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++) {
-        if (n < m->nmaster)
-            mfacts += c->cfact;
-        else
-            sfacts += c->cfact;
-    }
-
-	if (n == 0)
-		return;
-
-	if (n > m->nmaster)
-		mw = m->nmaster ? m->ww * m->mfact : 0;
-	else
-		mw = m->ww;
-	for (i = my = ty = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++)
-		if (i < m->nmaster) {
-            h = (m->wh - my) * (c->cfact / mfacts); 
-			resize(c, m->wx, m->wy + my, mw - (2*c->bw), h - (2*c->bw), 0);
-			my += HEIGHT(c);
-            mfacts -= c->cfact; 
-		} else {
-            h = (m->wh - ty) * (c->cfact / sfacts);
-			resize(c, m->wx + mw, m->wy + ty, m->ww - mw - (2*c->bw), h - (2*c->bw), 0);
-			ty += HEIGHT(c);
-            sfacts -= c->cfact; 
-		}
-}
 
 void
 togglebar(const Arg *arg)
