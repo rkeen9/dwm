76fc825a5f2e3687586f0771f8faba8123e30604
Robert Keen <robertkeen9@gmail.com>
2021-09-30 15:31:06 -0700

    added laptop branch

--- config.h
+++ config.h
@@ -7,8 +7,8 @@ static const unsigned int gappx     = 15;       /* gap pixel between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Source Code Pro:size=12" }; 
-static const char dmenufont[]       = "Source Code Pro:size=12";
+static const char *fonts[]          = { "Source Code Pro:size=16" }; 
+static const char dmenufont[]       = "Source Code Pro:size=16";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -92,14 +92,14 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *browsercmd[] = { "firefox", NULL};
+static const char *browsercmd[] = { "brave", NULL};
 static const char *spotifycmd[] = {"spotify", NULL};
 static const char *volumeup[]  = { "/home/robert/scripts/volume_up.sh", NULL };
 static const char *volumedown[]  = { "/home/robert/scripts/volume_down.sh", NULL };
 static const char *screenshot[] = { "flameshot", "gui", "-p", "/home/robert/images/screenshots", NULL };
 static const char *bluetooth[] = { "blueman-applet", "&", "blueman-manager", NULL };
-static const char *brightnessup[] = { "xbacklight", "-inc", "10", NULL };
-static const char *brightnessdown[] = { "xbacklight", "-dec", "10", NULL };
+static const char *brightnessup[] = { "sudo", "/home/robert/scripts/brightness_up.sh", NULL };
+static const char *brightnessdown[] = { "sudo", "/home/robert/scripts/brightness_down.sh", NULL };
 static const char *bluetoothmenu[] = { "st", "-e", "bluetoothctl", NULL };
 static const char *updatekeyboard[] = { "setxkbmap", "-option", "caps:swapescape", NULL };
 static const char *shutdown[] = { "sudo", "shutdown", "-P", "now", NULL };
@@ -115,6 +115,7 @@ static const char *weather[] = { "st", "-e", "/home/robert/scripts/weather.sh", 
 static const char *python[] = { "st", "-e", "python", NULL };
 static const char *newsboat[] = { "st", "-e", "newsboat", NULL };
 static const char *kanji[] = { "/home/robert/scripts/rtk_keyboard.sh", NULL };
+static const char *recordDesktop[] = { "st", "-e", "/home/robert/scripts/record_desktop.sh", NULL };
 
 static Key keys[] = {
 	/* modifier                     key                      function        argument */
@@ -149,6 +150,7 @@ static Key keys[] = {
     { MODKEY|ShiftMask,             XK_p,                    spawn,          {.v = python} },
 //  { MODKEY,                       XK_n,                    spawn,          {.v = newsboat} },
     { MODKEY|ShiftMask,             XK_n,                    spawn,          {.v = kanji} },
+    { MODKEY|ShiftMask,             XK_r,                    spawn,          {.v = recordDesktop} },
 	{ MODKEY,                       XK_b,                    togglebar,      {0} },
 	{ MODKEY,                       XK_k,                    focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_j,                    focusstack,     {.i = -1 } },
@@ -192,7 +194,7 @@ static Button buttons[] = {
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
-	{ ClkClientWin,         SUPER,          Button1,        movemouse,      {0} },
+	{ ClkClientWin,         MODKEY,          Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
 	{ ClkTagBar,            0,              Button1,        view,           {0} },
