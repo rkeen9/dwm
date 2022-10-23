278642bdc6cff998149efa32977b951f75511cd5
Rob Keen <robert@robertkeen.ca>
2022-10-23 11:17:57 -0700

    fixed icon sizing in status bar

--- config.h
+++ config.h
@@ -7,8 +7,8 @@ static const unsigned int gappx     = 15;       /* gap pixel between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Source Code Pro:size=8" }; 
-static const char dmenufont[]       = "Source Code Pro:size=8";
+static const char *fonts[]          = { "Source Code Pro:pixelsize=20:antialias=true:autohint=true" }; 
+static const char dmenufont[]       = "Source Code Pro:pixelsize=20:antialias=true:autohint=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
